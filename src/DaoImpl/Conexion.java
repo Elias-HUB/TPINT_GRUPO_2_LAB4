513e7686b26ffdeb71cb3d42df816589d963b58e
elias-hub <elias.valenzuela@alumnos.utn.frgp.com.ar>
2020-06-13 20:50:46 -0300

    Cambio Conexion

--- src/DaoImpl/Conexion.java
+++ src/DaoImpl/Conexion.java
@@ -4,8 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import daoImpl.Conexion;
-
 public class Conexion {
 	public static Conexion instancia;
 	private Connection connection;
