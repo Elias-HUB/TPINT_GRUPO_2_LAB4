8da6f853b6abfa0859e5ca75e5bad9de02cf2afb
elias-hub <elias.valenzuela@alumnos.utn.frgp.com.ar>
2020-07-09 02:30:20 -0300

    Se agregar notas vacias

--- src/Servelets/ServletNota.java
+++ src/Servelets/ServletNota.java
@@ -39,7 +39,7 @@ public class ServletNota extends HttpServlet {
 			String Curso = request.getParameter("ParamAlumnoXCursoDocente").toString();
 			int curso = Integer.parseInt(Curso);
 			session.setAttribute("CursoNotas", curso);
-			List<Calificacion> ListaCalificaciones = (ArrayList<Calificacion>) nDao.readNotasXAlumno(curso);			
+			List<Calificacion> ListaCalificaciones = (ArrayList<Calificacion>) nDao.readNotasXAlumno(curso);
 			request.setAttribute("ListaCalificaciones", ListaCalificaciones);
 			request.getRequestDispatcher("AlumnosPorCursoDocente.jsp").forward(request, response);
 		}
@@ -57,54 +57,54 @@ public class ServletNota extends HttpServlet {
 			request.getParameter("Alumno");
 			request.getParameter("slEstado");
 			Calificaciones = request.getParameterValues("CalificacionAlumno");
-			for (int i = 0; i < Calificaciones.length; i += 7) {
-				Calificacion calificacion = new Calificacion();
-				Alumno alumno = new Alumno();
-				alumno.setLegajo(Integer.parseInt(Calificaciones[i+0]));
-				calificacion.setAlumno(alumno);
+			if (Calificaciones != null) {
+				for (int i = 0; i < Calificaciones.length; i += 7) {
+					Calificacion calificacion = new Calificacion();
+					Alumno alumno = new Alumno();
+					alumno.setLegajo(Integer.parseInt(Calificaciones[i + 0]));
+					calificacion.setAlumno(alumno);
 
-				// Parcial1
-				if (Calificaciones[i+1].equals("")) {
-					calificacion.setParcial1(0);
+					// Parcial1
+					if (Calificaciones[i + 1].equals("")) {
+						calificacion.setParcial1(0);
+					} else {
+						calificacion.setParcial1(Integer.parseInt(Calificaciones[i + 1]));
+					}
+
+					// Parcial2
+					if (Calificaciones[i + 2].equals(""))
+						calificacion.setParcial2(0);
+					else
+						calificacion.setParcial2(Integer.parseInt(Calificaciones[i + 2]));
+
+					// Recu1
+					if (Calificaciones[i + 3].equals(""))
+						calificacion.setRecu1(0);
+					else
+						calificacion.setRecu1(Integer.parseInt(Calificaciones[i + 3]));
+
+					// Recu2
+					if (Calificaciones[i + 4].equals(""))
+						calificacion.setRecu2(0);
+					else
+						calificacion.setRecu2(Integer.parseInt(Calificaciones[i + 4]));
+
+					// Notafinal
+					if (Calificaciones[i + 5].equals(""))
+						calificacion.setNotafinal(0);
+					else
+						calificacion.setNotafinal(Integer.parseInt(Calificaciones[i + 5]));
+
+					calificacion.setEstado(Calificaciones[i + 6]);
+					String Curso = session.getAttribute("CursoNotas").toString();
+					int curso = Integer.parseInt(Curso);
+					calificacion.setIdCurso(curso);
+
+					nDao.update(calificacion);
 				}
-				else {
-					calificacion.setParcial1(Integer.parseInt(Calificaciones[i+1]));					
-				}
-
-				// Parcial2
-				if (Calificaciones[i+2].equals(""))
-					calificacion.setParcial2(0);
-				else
-					calificacion.setParcial2(Integer.parseInt(Calificaciones[i+2]));					
-
-				// Recu1
-				if (Calificaciones[i+3].equals(""))
-					calificacion.setRecu1(0);
-				else
-					calificacion.setRecu1(Integer.parseInt(Calificaciones[i+3]));
-					
-
-				// Recu2
-				if (Calificaciones[i+4].equals(""))
-					calificacion.setRecu2(0);
-				else
-					calificacion.setRecu2(Integer.parseInt(Calificaciones[i+4]));					
-
-				// Notafinal
-				if (Calificaciones[i+5].equals(""))
-					calificacion.setNotafinal(0);
-				else
-					calificacion.setNotafinal(Integer.parseInt(Calificaciones[i+5]));					
-
-				calificacion.setEstado(Calificaciones[i+6]);
-				String Curso = session.getAttribute("CursoNotas").toString();
-	            int curso = Integer.parseInt(Curso);
-	            calificacion.setIdCurso(curso);
-	            
-	            nDao.update(calificacion);
 			}
-			
-			List<Calificacion> ListaCalificaciones = (ArrayList<Calificacion>) nDao.readNotasXAlumno(Integer.parseInt(session.getAttribute("CursoNotas").toString()));			
+			List<Calificacion> ListaCalificaciones = (ArrayList<Calificacion>) nDao
+					.readNotasXAlumno(Integer.parseInt(session.getAttribute("CursoNotas").toString()));
 			request.setAttribute("ListaCalificaciones", ListaCalificaciones);
 			request.getRequestDispatcher("AlumnosPorCursoDocente.jsp").forward(request, response);
 		}
